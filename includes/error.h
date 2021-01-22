3bedcbd069b36b52f9e7bb773711d37b2d7e7ce8
rumellie <redlapt@gmail.com>
2021-01-22 21:41:29 +0300

    asm_v1

--- includes/error.h
+++ includes/error.h
@@ -3,20 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   error.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lseema <lseema@student.21-school.ru>       +#+  +:+       +#+        */
+/*   By: mellie <mellie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 20:58:15 by lseema            #+#    #+#             */
-/*   Updated: 2021/01/21 21:25:37 by lseema           ###   ########.fr       */
+/*   Updated: 2021/01/22 21:37:20 by mellie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
-# define COREWAR_USAGE "USAGE: ./corewar [-dump nbr_cycles] [[-n number] champion1.cor] ..."
-# define ERR_1_MAX_PLAYERS "ERROR: The number of players is more than allowed."
-# define ERR_2_DUP_ID_PLAYER "ERROR: ID_player must be unique."
-# define ERR_3_READING_FAILED "ERROR: Reading is failed."
-# define ERR_4_CODE_MAXSIZE "ERROR: The size of code is more than MaxSize."
-# define ERR_5_INV_CODESIZE "ERROR: The size of code != SIZE in the .cor file."
+# define COREWAR_USAGE			"USAGE: ./corewar [-dump nbr_cycles] [[-n number] champion1.cor] ..."
+# define ERR_1_MAX_PLAYERS		"ERROR: The number of players is more than allowed."
+# define ERR_2_DUP_ID_PLAYER	"ERROR: ID_player must be unique."
+# define ERR_3_READING_FAILED	"ERROR: Reading is failed."
+# define ERR_4_CODE_MAXSIZE		"ERROR: The size of code is more than MaxSize."
+# define ERR_5_INV_CODESIZE		"ERROR: The size of code != SIZE in the .cor file."
+# define ASM_USAGE				"Usage: ./asm <sourcefile.s>."
+# define ERROR_WRONG_EXTENSION	"Expected *.s file."
+# define SYNTAX_ERROR			"Can't compile."
 #endif
