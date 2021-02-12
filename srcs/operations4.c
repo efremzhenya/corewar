d791fe8ce2615d3d209f3d6abb7a97ba890e965d
lseema <lseema@student.21-school.ru>
2021-02-12 21:05:05 +0300

    добавил условие для выполнения aff

--- srcs/operations4.c
+++ srcs/operations4.c
@@ -6,7 +6,7 @@
 /*   By: lseema <lseema@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 23:15:06 by lseema            #+#    #+#             */
-/*   Updated: 2021/01/21 21:50:46 by lseema           ###   ########.fr       */
+/*   Updated: 2021/02/12 21:04:19 by lseema           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ void	op_aff(t_corewar **corewar, t_carrage *carrage)
 
 	offset = carrage->pc + sizeof(t_op_type) + sizeof(t_arg_type);
 	reg_value = carrage->registers[read_byte((*corewar)->arena, offset)];
-	ft_putstr("Aff: ");
-	ft_putchar((char)reg_value % 256);
-	ft_putstr("\n");
+	if (!(*corewar)->cw_args->visual && (*corewar)->cw_args->dump == -1)
+	{
+		ft_putstr("Aff: ");
+		ft_putchar((char)reg_value % 256);
+		ft_putstr("\n");
+	}
 }
 
 void	op_nop(t_corewar **corewar, t_carrage *carrage)
